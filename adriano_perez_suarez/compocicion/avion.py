22e67f4aee80071a4e264a9067df511e1cb96c98
adriano <adrianosegudop@gmail.com>
2025-12-15 09:32:16 -0500

    terminado

--- adriano_perez_suarez/compocicion/avion.py
+++
@@ -1,9 +0,0 @@
-class Motor:
-    def __init__(self, tipo: str, potencia_newtons: float):
-        self.tipo = tipo
-        self.potencia_newtons = potencia_newtons
-
-class Aeronave:
-    def __init__(self, modelo: str, motor: Motor = None): # type: ignore
-        self.modelo = modelo
-        self.motor = motor if motor else Motor("turbofan por defecto", 0.0)
